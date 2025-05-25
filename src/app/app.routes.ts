9c113e2a4f28e471e3e7579e94776948ce4feafc
Jenya Shvartsman <jenyas@1touch.io>
2025-05-24 22:10:16 -0400

    init pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Authentication routes
+  {
+    path: 'sign-in',
+    loadComponent: () => import('./pages/sign-in/sign-in.component').then((c) => c.SignInComponent),
+  },
+  // Secure routes
+  {
+    path: 'dashboard',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'cameras',
+      },
+      {
+        path: 'cameras',
+        loadComponent: () =>
+          import('./pages/cameras/cameras.component').then((c) => c.CamerasComponent),
+      },
+      // Fallback route
+      {
+        path: '**',
+        redirectTo: 'cameras',
+      },
+    ],
+  },
+  // Fallback route
+  {
+    path: '**',
+    redirectTo: 'sign-in',
+  },
+];
