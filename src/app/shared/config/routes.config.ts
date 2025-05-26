c1a42b773db430ee205fe0337b11ce8883b522fb
Jenya Shvartsman <jenyas@1touch.io>
2025-05-26 14:45:26 -0400

    update routes config documentation

--- src/app/shared/config/routes.config.ts
+++ src/app/shared/config/routes.config.ts
@@ -1,2 +1,8 @@
+/*
+ * This file contains the route configurations for the application.
+ * It defines the paths for various routes used in the application.
+ * example: 'this.router.navigate([dashboardRouteConfig])' or 'readonly dashboardRoute = dashboardRouteConfig;'
+ */
+
 export const signInRouteConfig = '/sign-in';
 export const dashboardRouteConfig = '/dashboard';
